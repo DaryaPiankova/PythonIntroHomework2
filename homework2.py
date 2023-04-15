75fc1185e74214a276d4f9ef9e33eac625a075e5
Daria <darya.piankova@yandex.ru>
2023-04-15 16:17:21 +0500

    task12 new

--- homework2.py
+++ homework2.py
@@ -2,12 +2,9 @@ summa = int(input('Введите первое число: '))
 multipl = int(input('Введите второе число: '))
 num1 = 0
 num2 = 0
-for i in range(multipl):
-    for i in range(summa):
+for num1 in range(multipl):
+    for num2 in range(summa):
         if num1*num2 == multipl and num1+num2 == summa:
             print(num1, num2)
-            break
-        else:
-            num2 += 1
-    num1 += 1
-    num2 = 0
+           
+      
